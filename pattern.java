d4e083f96efa85875c0188cb6346dceb57191f39
Dhrumil Thakar <dhruvmilthakar>
2025-12-18 21:46:37 +0530

    pattern problem

---
+++ pattern.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class pattern{
+
+    public static void printPattern1(int n){
+        for(int i=0;i<n;i++){
+            for(int j =0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printPattern2(int n){
+        for(int i=0;i<=n;i++){
+            for(int j =0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printPattern3(int n){
+        for(int i=1;i<=n;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+   
+    public static void printPattern4(int n){
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        printPattern4(4);
+    }
+}
